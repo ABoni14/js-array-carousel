e303117dbd9c72e5c5215343ab2eb73d52eeec66
ABoni14 <aboni150@gmail.com>
2021-10-27 15:05:53 +0200

    dichiarate le variabili, funzione click su arrow

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// ARRAY CONSEGNATI
 const items = [
   'img/01.jpg',
   'img/02.jpg',
@@ -21,3 +22,33 @@ const text = [
   'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
   'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,'
 ];
+// //ARRAY CONSEGNATI
+
+// DICHIARO LE VARIABILI
+const item = document.getElementsByClassName("item");
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+
+// CONTATORE PER CICLARE LE FOTO
+let contatore = 0;
+
+// FUNZIONI BOTTONE
+prev.addEventListener("click",function(){
+  console.log(item[contatore]);
+  item[contatore].classList.remove("active");
+  console.log(item[contatore]);
+  contatore--;
+  if(contatore < 0) contatore = item.length -1;
+  console.log(contatore);
+  item[contatore].classList.add("active")
+});
+
+next.addEventListener("click", function(){
+  console.log(item[contatore]);
+  item[contatore].classList.remove("active");
+  console.log(item[contatore]);
+  contatore++;
+  if(contatore > item.length -1) contatore = 0;
+  console.log(contatore);
+  item[contatore].classList.add("active");
+});
